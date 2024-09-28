8e9728610f1124ee385fe465858350c722d13f71
Nadav09088 <nadavy666@gmail.com>
2024-09-28 22:02:58 +0300

    add "!"

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected successfully"))
+  .then(() => console.log("MongoDB connected successfully!"))
   .catch((err) => console.error("MongoDB connection error: ", err));
 
 app.use(logger("dev"));
